334404012169b2c2f8c9494d8987dcb56c3f8fef
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-03-04 20:33:51 -0300

    feat(wip): header component creating and styles

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.appTitle}>BEBA ÁGUA 💧</Text>
+        <Text style={styles.appSubtitle}> HIDRATAÇÃO DIÁRIA</Text>
+      </View>
+
+      <Text>Corpo do texto</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +20,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20
   },
+  header: {
+    marginTop: 60,
+    alignItems: 'center',
+  },
+  appTitle: {
+    fontSize: 32,
+    fontWeight: '900',
+    color: '#0369A1'
+  },
+  appSubtitle: {
+    fontSize: 12,
+    fontWeight: '700',
+    color: '#38BDF8',
+    letterSpacing: 2,
+    marginTop: 4
+  }
 });
