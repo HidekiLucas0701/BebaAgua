e0fd066be6e2939aa25942381cfd62b9a1d38399
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-03-04 20:58:57 -0300

    feat(wip): water level circle with styles

--- App.tsx
+++ App.tsx
@@ -9,6 +9,11 @@ export default function App() {
         <Text style={styles.appSubtitle}> HIDRATAÇÃO DIÁRIA</Text>
       </View>
 
+      <View style={styles.content}>
+        <View style={styles.outerCircle}>
+          <View style={styles.waterLevel} />
+        </View>
+      </View>
       <Text>Corpo do texto</Text>
       <StatusBar style="auto" />
     </View>
@@ -38,5 +43,34 @@ const styles = StyleSheet.create({
     color: '#38BDF8',
     letterSpacing: 2,
     marginTop: 4
+  },
+  content: {
+    alignItems: 'center',
+    width: '100%'
+  },
+  outerCircle: {
+    width: 220,
+    height: 220,
+    borderRadius: 110,
+    borderWidth: 8,
+    borderColor: '#BAE6FD',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    elevation: 100,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 5},
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+  },
+  waterLevel: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    height: 60,
+    backgroundColor: '#0EA5E9',
+    opacity: 0.3
   }
 });
